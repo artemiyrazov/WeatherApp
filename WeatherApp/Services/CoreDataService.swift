d8fb90d17140dc2f6a7a2f49ffbfc814dcff2e62
artemiy <->
2020-07-31 11:21:29 +0300

    Create data model and singleton class for CoreData stack

---
+++ WeatherApp/Services/CoreDataService.swift
@@ -0,0 +1,38 @@
+//
+//	CoreDataService.swift
+// 	WeatherApp
+//
+
+import Foundation
+import CoreData
+
+class CoreDataService {
+    
+    static let shared = CoreDataService()
+    private init() {}
+    
+    var mainContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private func saveContext () {
+        if mainContext.hasChanges {
+            do {
+                try mainContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
